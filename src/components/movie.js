c79059399f37ccc7d89bd6b1bbe4615d5366b5c1
Anthony <anthony.venisse@businessagile.eu>
2017-01-31 10:59:43 +0100

    Add semantic-ui react and download button

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Grid,Segment,Button} from 'semantic-ui-react';
 import ReactPlayer from 'react-player';
 import Header from './header';
 
@@ -9,31 +10,24 @@ class Movie extends Component {
         return (
             <div>
                 <Header />
-                <div>
-                <h2>{this.props.activeMovie.title}</h2>
-                <table>
-                    <tbody>
-                        <div>
-                            <tr>
-                                <td>Director : {this.props.activeMovie.director}</td>
-                            </tr>
-                            <tr>
-                                <td>Actors : {this.props.activeMovie.actors}</td>
-                            </tr>
-                            <tr>
-                                <td>Genre : {this.props.activeMovie.genre}</td>
-                            </tr>
-                            <tr>
-                                <td>Release Date : {this.props.activeMovie.release_date}</td>
-                            </tr>
-                        </div>
-                    <tr>
+                <Grid columns={2}>
+                    <Grid.Column width={6}>
                         <td rowSpan="7" colSpan="2"><img src={this.props.activeMovie.cover} style={{width: 400, height: 600}}/></td>
-                    </tr>
-
-                    </tbody>
-                </table>
-                </div>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h2>{this.props.activeMovie.title}</h2>
+                        <h4>Director : {this.props.activeMovie.director}</h4>
+                        <h4>Actors : {this.props.activeMovie.actors}</h4>
+                        <h4>Genre : {this.props.activeMovie.genre}</h4>
+                        <h4>Release Date : {this.props.activeMovie.release_date}</h4>
+                        <Segment>
+                            <h5>Synopsis</h5>
+                            <div>{this.props.activeMovie.synopsis}</div>
+                        </Segment>
+                        <div></div>
+                        <Button content='Download' icon='download icon' color='green' labelPosition='left' />
+                    </Grid.Column>
+                </Grid>
             </div>
         );
     }
